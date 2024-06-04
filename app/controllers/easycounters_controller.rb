cc4acada4a27e889075d74f281b8674c1d1cbd6e
nankotsu-ballista <69568626+nankotsu-ballista@users.noreply.github.com>
2024-06-04 21:45:33 +0000

    ssibal

---
+++ app/controllers/easycounters_controller.rb
@@ -0,0 +1,21 @@
+class EasycountersController < ApplicationController
+  def index
+    # Your code for the index action goes here
+  end
+
+  def show
+    # Your code for the show action goes here
+  end
+
+  def create
+    # Your code for the create action goes here
+  end
+
+  def update
+    # Your code for the update action goes here
+  end
+
+  def destroy
+    # Your code for the destroy action goes here
+  end
+end
--- app/controllers/lands_controller.rb
+++
@@ -1,8 +0,0 @@
-def create
-  @land = current_user.lands.build(post_params)
-  if @post.save
-    redirect_to current_user
-  else
-    render root_path
-  end
-end

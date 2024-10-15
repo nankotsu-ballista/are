dc702da01c64653296110d5e632ce25df42825a1
kaede3128739 <69568626+nankotsu-ballista@users.noreply.github.com>
2024-10-15 03:08:09 +0000

    just

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_26_020558) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_26_020558) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -158,15 +158,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_26_020558) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "meta", force: :cascade do |t|
-    t.integer "IDmeta"
-    t.string "name"
-    t.integer "IDGame"
-    t.string "setumei"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "metamons", force: :cascade do |t|
     t.string "name"
     t.string "setumei"
@@ -204,14 +195,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_26_020558) do
     t.integer "temp_id"
     t.integer "metamon_id"
     t.integer "SC"
-    t.integer "csgo"
-  end
-
-  create_table "situations", force: :cascade do |t|
-    t.string "situname"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "temps", force: :cascade do |t|
